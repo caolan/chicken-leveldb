47be0f55602f820b3ee6453e0833e690669d5fbd
Caolan McMahon <caolan@caolanmcmahon.com>
2014-03-26 14:59:13 +0000

    first commit

---
+++ level.scm
@@ -0,0 +1,26 @@
+(module level (test)
+
+(import scheme chicken foreign)
+
+(foreign-declare "#include <iostream>")
+(foreign-declare "#include \"leveldb/db.h\"")
+
+(define test
+  (foreign-lambda* int ()
+                 "leveldb::DB* db;
+                  leveldb::Options options;
+                  leveldb::Status status;
+
+                  options.create_if_missing = true;
+
+                  status = leveldb::DB::Open(options, \"./testdb\", &db);
+
+                  std::string key = \"abc\";
+                  std::string value = \"123\";
+                  std::string ret;
+                  std::cout << value;
+                  status = db->Put(leveldb::WriteOptions(), key, value);
+                  status = db->Get(leveldb::ReadOptions(), key, &ret);
+                  std::cout << ret;
+
+                  C_return(0);")))
