d99066b1524d4272dbd1cb18a55c511d220df6ed
Caolan McMahon <caolan@caolanmcmahon.com>
2014-04-20 12:59:18 +0100

    remove leveltest.scm

--- leveltest.scm
+++
@@ -1,9 +0,0 @@
-(use leveldb)
-
-(define db (open-db "./testdb"))
-(define key (list->string '(#\f #\o #\o #\nul)))
-(define val (list->string '(#\b #\a #\r #\nul)))
-
-(db-put db key val)
-(write (db-get db key))
-(close-db db)
